4b254cd162350adc8cbbc5730b1a436756bcbbe4
Anthony Goeckner <goecknea@oregonstate.edu>
2025-03-04 22:36:52 -0800

    code smell

--- Microservice A/StorageManager.py
+++ Microservice A/StorageManager.py
@@ -5,21 +5,32 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
+
+def write_to_csv(msg):
+    csv = open("./maintenance_log.csv", "a")
+    csv.write(str(msg).split("'")[1])
+    csv.write("\n")
+    csv.close()
+
+
 while True:
     try:
         msg = socket.recv()
 
+        # if the message says exit, this program will close
         if msg == b'exit':
             socket.send(b'closing')
             break
 
+        # checks if the message can be split into 3
+        # writes, sends 0 if true, else sends 1
         if len(str(msg).split(";")) == 3:
-            csv = open("./maintenance_log.csv", "a")
-            csv.write(str(msg).split("'")[1])
-            csv.write("\n")
+            write_to_csv(str(msg))
             socket.send_string("0")
-            csv.close()
+
         else:
             socket.send_string("1")
+
+    # sends a -1 for any exceptions
     except:
         socket.send_string("-1")
